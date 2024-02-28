a7c541502541b41ac743ea41d9a750a1afc927d5
loryn-kinsa <loryn@kinsahealth.com>
2024-02-27 17:01:04 -0800

    init

---
+++ questions.py
@@ -0,0 +1,52 @@
+questions = [
+    {
+    'order': 1,
+    'question': "",
+    'options': ""   
+    },
+    {
+    'order': 2,
+    'question': "",
+    'options': ""   
+    },
+    {
+    'order': 3,
+    'question': "",
+    'options': ""   
+    },
+    {
+    'order': 4,
+    'question': "",
+    'options': ""   
+    },
+    {
+    'order': 5,
+    'question': "",
+    'options': ""   
+    },
+    {
+    'order': 6,
+    'question': "",
+    'options': ""   
+    },
+    {
+    'order': 7,
+    'question': "",
+    'options': ""   
+    },
+    {
+    'order': 8,
+    'question': "",
+    'options': ""   
+    },
+    {
+    'order': 9,
+    'question': "",
+    'options': ""   
+    },
+    {
+    'order': 10,
+    'question': "",
+    'options': ""   
+    },
+]
